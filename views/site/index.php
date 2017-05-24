266bc9fcc19a9af1a8042da5e0f1fce18f37b30a
Cyanoluck <cyanoluck@gmail.com>
2017-05-24 16:40:32 +0300

    f

--- views/site/index.php
+++ views/site/index.php
@@ -151,57 +151,34 @@ $this->title = 'Итернет-магазин техники';
 
         <div class="col-md-3 categories">
 
-            <ul class="hidden-xs hidden-sm">
+            <div class="mobile-menu-area hidden-sm hidden-xs">
+                <div class="">
 
-                <h3>Категории</h3>
-
-                <?php foreach ($category as $cat) { ?>
-
-                    <!--                    <li><a href="#">--><? //= $cat->name ?><!--</a></li>-->
-
+                    <div class="mobile-menu">
+                        <nav id="dropdown">
+                            <ul>
 
 
-                    <div class="btn-group dropdown">
+                                <?php foreach ($category as $cat): ?>
+                                    <? $categ = Category::find()->where(['parent_id' => $cat['id']])->all(); ?>
+                                    <button class="accordion" ><?= $cat['name'] ?></button>
+                                    <div class="panel">
+                                        <ul>
+                                            <?php foreach ($categ as $c): ?>
+                                                <li>
+                                                    <a href="<?= Url::to(['site/category', 'id' => $c['id']]) ?>"><?= $c['name']; ?></a>
+                                                </li>
+                                            <?php endforeach; ?>
+                                        </ul>
+                                    </div>
+                                <?php endforeach; ?>
 
-                        <a href="<?= Url::to(['site/category', 'id' => $cat->id]) ?>" class="btn"
-
-                           data-label-placement><?= $cat->name ?></a>
-
-
-
-                        <a data-toggle="dropdown" data-hover="dropdown" class="btn dropdown-toggle"><span
-
-                                class="fa fa-angle-right" style="position: absolute;right: 10px;font-size: 15px"></span></a>
-
-
-
-                        <ul class="dropdown-menu pull-middle pull-right pull-middle-true">
-
-                            <?php
-
-                            $categ = Category::find()->where(['parent_id' => $cat['id']])->all();
-
-                            foreach ($categ as $c) {
-
-                                //$count = Product::find()->where(['category_id' => $c->id])->count();
-
-                                ?>
-
-                                <li><a href="<?= Url::to(['site/category', 'id' => $c['id']]) ?>"><?= $c['name']; ?></a>
-
-                                </li>
-
-                            <?php } ?>
-
-                        </ul>
-
+                            </ul>
+                        </nav>
                     </div>
 
-
-
-                <?php } ?>
-
-            </ul>
+                </div>
+            </div>
 
 
 
